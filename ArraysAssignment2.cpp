2b470c12e25ac32c1be53f8c00c880a7a71a54ec
DValdovinos4 <domanikvaldovinos@gmail.com>
2026-03-14 13:17:25 -0700

    Update ArraysAssignment2.cpp

--- ArraysAssignment2.cpp
+++ ArraysAssignment2.cpp
@@ -12,10 +12,16 @@ int main() {
 
     cout << "Enter up to 20 scores (0-100). Enter -1 to stop:\n";
 
-    // Store scores and keep track of how many are entered
-    // Report perfect scores using the helper function
-  // COMPLETE THE FUNCTION BELOW
+    for(int i=0;i<20;i++){
+        cin>>input;
+        if(i==-1){
+            break;
+        }
+        scores[i]=input;
+        count++;
+    }
   int perfectOnes = countPerfect(scores, count);
+
     
     cout << "\nYou entered " << count << " scores." << endl;
     cout << "Perfect scores (100): " << perfectOnes << endl;
@@ -26,6 +32,11 @@ int main() {
 // Value-returning function to count scores of 100
 int countPerfect(const int scores[], int size) {
     int totalPerfect = 0;
-    //COMPLETE THE CODE 
+    for(int i=0;i<size;i++){
+        if(scores[i]==100){
+            totalPerfect++;
+        }
+
+ 
     return totalPerfect;
 }
